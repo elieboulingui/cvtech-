80ec545f4a34a984fb5182abe3cf802347c23d71
elieboulingui <elieboulingui2@gmail.com>
2024-07-22 17:45:24 +0100

    j ai ajoute le filigrane

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-"use client";
-import React from 'react';
-import { Page, Text, View, Document, StyleSheet , PDFViewer} from '@react-pdf/renderer';
+ "use client"
+import React, { useState } from 'react';
+import { View, PDFViewer } from '@react-pdf/renderer';
 import Pdf from "../app/components/Pdf";
-export default function Home() {
+
+const Home = () => {
+  const [nom, setNom] = useState('');
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
+ 
+
+
   return (
     <main className="w-screen h-screen">
-      <PDFViewer className= "w-full h-full"> 
-        <Pdf/>
-      </PDFViewer>
+     
+        <PDFViewer className="w-full h-full" showToolbar={false} >
+          <Pdf nom={nom} />
+        </PDFViewer>
     </main>
   );
-}
+};
+
+export default Home;
